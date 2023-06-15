3b886495dba630d80d4d18c6b22a6ff10657d79b
juan <jfcastropalma@gmail.com>
2023-06-14 20:59:58 -0400

    integracion con templates phantom

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'phantom/index'
+  get 'phantom/generic'
+  get 'phantom/elements'
   resources :libros
   get 'welcome/index'
+  get "welcome", to: 'welcome#index'
+  get "index", to: 'phantom#index'
+  get "generic", to: 'phantom#generic'
+  get "elements", to: 'phantom#elements'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
